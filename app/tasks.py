98c43dc051c72cbc911268ff828f9cfcc2bd565c
Paras <paras.gupta@sqrrl.in>
2021-12-01 12:00:24 +0530

    granular level commiting after updating available dates for car

--- app/tasks.py
+++ app/tasks.py
@@ -3,6 +3,9 @@ from datetime import date, timedelta
 from app.celery import wdb_session, celery_app, DBTask
 from app.models import Car
 from app.utils import chunks
+from app.log import get_logger
+
+LOG = get_logger()
 
 
 @celery_app.task(base=DBTask)
@@ -19,14 +22,17 @@ def update_available_dates():
 @celery_app.task(base=DBTask)
 def update_available_dates_for_a_car(car_ids=[]):
     for car_id in car_ids:
-        car = wdb_session.query(Car).get(car_id)
-        available_dates = sorted(car.availability_for_60_days)
-        if available_dates[0] < date.today():
-            # need to remove yesterday's available date
-            available_dates.pop(0)
+        try:
+            car = wdb_session.query(Car).get(car_id)
+            available_dates = sorted(car.availability_for_60_days)
+            if available_dates[0] < date.today():
+                # need to remove yesterday's available date
+                available_dates.pop(0)
 
-        # add next available dates after 59th day
-        available_dates.append(date.today() + timedelta(days=59))
-        car.availability_for_60_days = available_dates
-
-    wdb_session.commit()
+            # add next available dates after 59th day
+            available_dates.append(date.today() + timedelta(days=59))
+            car.availability_for_60_days = available_dates
+            wdb_session.commit()
+        except Exception as ex:
+            LOG.error("Error occurred while updating available dates: ", ex)
+            wdb_session.rollback()
